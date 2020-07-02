e5055ede2e80539daaf842b5f1f5ce13467abdc0
Marat Reymers <marat.maratori@gmail.com>
2020-07-03 00:55:43 +0300

    Do not expect name for single parameter

--- pkg/idescriptive/testdata/strict_false/bad_interface.go
+++ pkg/idescriptive/testdata/strict_false/bad_interface.go
@@ -1,7 +1,7 @@
 package strict_false
 
-type BadSingleParam interface {
-	NoReturn(int)              // want `missing name for incoming parameter int in method NoReturn\(int\)`
-	SingleReturn(bool) (x int) // want `missing name for incoming parameter bool in method SingleReturn\(bool\) \(x int\)`
+type BadTwoParams interface {
+	NoReturn(int, int)                 // want `missing name for incoming parameter int in method NoReturn\(int, int\)` `missing name for incoming parameter int in method NoReturn\(int, int\)`
+	SingleReturn(bool, string) (x int) // want `missing name for incoming parameter bool in method SingleReturn\(bool, string\) \(x int\)` `missing name for incoming parameter string in method SingleReturn\(bool, string\) \(x int\)`
 	DoubleReturn(Empty) (x int, err error)
 }
--- pkg/idescriptive/testdata/strict_false/good_interface.go
+++ pkg/idescriptive/testdata/strict_false/good_interface.go
@@ -8,8 +8,16 @@ type NoParams interface {
 	DoubleReturn() (x int, err error)
 }
 
-type SingleParam interface {
-	NoReturn(a int)
-	SingleReturn(b bool) (c int)
+type OneParam interface {
+	NoReturn(int)
+	SingleReturn(bool) (c int)
+	DoubleReturn(Empty) (d int, err error)
+}
+
+type TwoParams interface {
+	NoReturn(a, b int)
+	NoReturn2(a string, b int)
+	SingleReturn(b, c bool) (d int)
+	SingleReturn2(b bool, c string) (d int)
 	DoubleReturn(Empty) (d int, err error)
 }
